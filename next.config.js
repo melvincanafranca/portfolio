86cd40c425426ed6b3cf5cf3844a9e95e9a1dcd0
melvincanafranca <melvincanafranca@gmail.com>
2022-08-04 23:38:37 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["vercel.com/"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
